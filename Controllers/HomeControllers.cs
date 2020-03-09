64fb878a03818113af8a5e4b766e4e996def29a4
Dana <dcluukko@gmail.com>
2020-03-09 11:29:47 -0700

    successfully connected MadLibVariable with HomeControllers, Form.cshtml, and MadLib.cshtml

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -1,1 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Madlibz.Models;
 
+namespace MadLibz.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+    [Route("MadLib")]
+    public ActionResult MadLib(string noun, string verb)
+    {
+      MadLibVariable myMadLibVariable = new MadLibVariable();
+      myMadLibVariable.Noun = noun;
+      myMadLibVariable.Verb = verb;
+      return View(myMadLibVariable);
+    }
+  }
+}
---
+++ Models/MadLibVariable.cs
@@ -0,0 +1,8 @@
+namespace Madlibz.Models
+{
+  public class MadLibVariable
+  {
+    public string Noun {get; set;}
+    public string Verb {get; set;}
+  }
+}
