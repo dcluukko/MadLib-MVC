f34db975846efde04fc15a64a2e0139ed136dfa4
Dana <dcluukko@gmail.com>
2020-03-09 13:59:24 -0700

    set up cshtml, OSayVariable, and homecontroller

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -9,11 +9,27 @@ namespace MadLibz.Controllers
     [Route("/OSayForm")]
     public ActionResult OSayForm() { return View(); }
     [Route("MadLib")]
-    public ActionResult OSay(string noun1, string verb1)
+    public ActionResult OSay(string verb1, string noun1, string adverb, string verbPast, string pluralNoun1, string pluralNoun2, string adjective1, string pluralNoun3, string verbIng, string color, string pluralNoun4, string noun2, string noun3, string interjection, string adjective2, string verb2, string adjective3, string adjective4)
     {
       OSayVariable myOSayVariable = new OSayVariable();
-      myOSayVariable.Noun1 = noun1;
       myOSayVariable.Verb1 = verb1;
+      myOSayVariable.Noun1 = noun1;
+      myOSayVariable.Adverb = adverb;
+      myOSayVariable.VerbPast = verbPast;
+      myOSayVariable.PluralNoun1 = pluralNoun1;
+      myOSayVariable.PluralNoun2 = pluralNoun2;
+      myOSayVariable.Adjective1 = adjective1;
+      myOSayVariable.PluralNoun3 = pluralNoun3;
+      myOSayVariable.VerbIng = verbIng;
+      myOSayVariable.Color = color;
+      myOSayVariable.PluralNoun4 = pluralNoun4;
+      myOSayVariable.Noun2 = noun2;
+      myOSayVariable.Noun3 = noun3;
+      myOSayVariable.Interjection = interjection;
+      myOSayVariable.Adjective2 = adjective2;
+      myOSayVariable.Verb2 = verb2;
+      myOSayVariable.Adjective3 = adjective3;
+      myOSayVariable.Adjective4 = adjective4;
       return View(myOSayVariable);
     }
   }
--- Models/OSayVariable.cs
+++ Models/OSayVariable.cs
@@ -6,10 +6,10 @@ namespace Madlibz.Models
     public string Noun1 {get; set;}
     public string Adverb {get; set;}
     public string VerbPast {get; set;}
-    public string PluralNoun {get; set;}
+    public string PluralNoun1 {get; set;}
     public string PluralNoun2 {get; set;}
     public string Adjective1 {get; set;}
-    public string PuralNoun3 {get; set;}
+    public string PluralNoun3 { get; set;}
     public string VerbIng {get; set;}
     public string Color {get; set;}
     public string PluralNoun4 {get; set;}
