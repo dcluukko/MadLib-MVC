a26fd6a49314bff2a9c342092285264c03bd92bf
Dana <dcluukko@gmail.com>
2020-03-09 16:45:43 -0700

    connected multiple pages from a home page

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -6,6 +6,8 @@ namespace MadLibz.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
+    [Route("/Home")]
+    public ActionResult Home() { return View(); }
     [Route("/OSayForm")]
     public ActionResult OSayForm() { return View(); }
     [Route("/OSay")]
@@ -31,6 +33,32 @@ namespace MadLibz.Controllers
       myOSayVariable.Adjective3 = adjective3;
       myOSayVariable.Adjective4 = adjective4;
       return View(myOSayVariable);
+    }
+    [Route("/StoryForm")]
+    public ActionResult StoryForm() { return View(); }
+    [Route("/Story")]
+    public ActionResult Story(string verb1, string noun1, string adverb, string verbPast, string pluralNoun1, string pluralNoun2, string adjective1, string pluralNoun3, string verbIng, string color, string pluralNoun4, string noun2, string noun3, string interjection, string adjective2, string verb2, string adjective3, string adjective4)
+    {
+      StoryVariable myStoryVariable = new StoryVariable();
+      myStoryVariable.Verb1 = verb1;
+      myStoryVariable.Noun1 = noun1;
+      myStoryVariable.Adverb = adverb;
+      myStoryVariable.VerbPast = verbPast;
+      myStoryVariable.PluralNoun1 = pluralNoun1;
+      myStoryVariable.PluralNoun2 = pluralNoun2;
+      myStoryVariable.Adjective1 = adjective1;
+      myStoryVariable.PluralNoun3 = pluralNoun3;
+      myStoryVariable.VerbIng = verbIng;
+      myStoryVariable.Color = color;
+      myStoryVariable.PluralNoun4 = pluralNoun4;
+      myStoryVariable.Noun2 = noun2;
+      myStoryVariable.Noun3 = noun3;
+      myStoryVariable.Interjection = interjection;
+      myStoryVariable.Adjective2 = adjective2;
+      myStoryVariable.Verb2 = verb2;
+      myStoryVariable.Adjective3 = adjective3;
+      myStoryVariable.Adjective4 = adjective4;
+      return View(myStoryVariable);
     }
   }
 }
---
+++ Models/StoryVariable.cs
@@ -0,0 +1,24 @@
+namespace Madlibz.Models
+{
+  public class StoryVariable
+  {
+    public string Verb1 {get; set;}
+    public string Noun1 {get; set;}
+    public string Adverb {get; set;}
+    public string VerbPast {get; set;}
+    public string PluralNoun1 {get; set;}
+    public string PluralNoun2 {get; set;}
+    public string Adjective1 {get; set;}
+    public string PluralNoun3 { get; set;}
+    public string VerbIng {get; set;}
+    public string Color {get; set;}
+    public string PluralNoun4 {get; set;}
+    public string Noun2 {get; set;}
+    public string Noun3 {get; set;}
+    public string Interjection {get; set;}
+    public string Adjective2 {get; set;}
+    public string Verb2 {get; set;}
+    public string Adjective3 {get; set;}
+    public string Adjective4 {get; set;}
+  }
+}
